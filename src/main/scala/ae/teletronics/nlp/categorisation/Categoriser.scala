8f2ce9f92d9d6c00f49c9c61ad9a668833447a96
hhravn <hhravn@gmail.com>
2016-03-31 11:54:54 +0200

    added categoriser classes

---
+++ src/main/scala/ae/teletronics/nlp/categorisation/Categoriser.scala
@@ -0,0 +1,10 @@
+package ae.teletronics.nlp.categorisation
+
+/**
+  * Created by hhravn on 31/03/16.
+  */
+class Categoriser(val categories: Array[Category]) {
+  def categorise(text: String) : Array[String] = {
+    return null
+  }
+}
---
+++ src/main/scala/ae/teletronics/nlp/categorisation/Category.scala
@@ -0,0 +1,7 @@
+package ae.teletronics.nlp.categorisation
+
+/**
+  * Created by hhravn on 31/03/16.
+  */
+case class Category(name: String, words: Array[String]) {
+}
