c714a3694dcc49b6f7fec44e1b7047b3baf3beca
hhravn <hhravn@gmail.com>
2016-03-31 12:04:20 +0200

    disable javadoc for now

---
+++ src/test/scala/ae/teletronics/nlp/categorisation/CategoriserTest.scala
@@ -0,0 +1,15 @@
+package ae.teletronics.nlp.categorisation
+
+/**
+  * Created by hhravn on 31/03/16.
+  */
+import org.junit._
+import Assert._
+
+@Test
+class CategoriserTest {
+
+  @Test
+  def testOK() = assertTrue(true)
+
+}
