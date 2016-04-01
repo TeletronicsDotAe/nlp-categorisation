c65c07376c80d07fb4171223e1d83e7b77cae271
hhravn <hhravn@gmail.com>
2016-04-01 10:54:44 +0200

    migrate to hamcrest tests

--- src/test/scala/ae/teletronics/nlp/categorisation/CategoriserTest.scala
+++ src/test/scala/ae/teletronics/nlp/categorisation/CategoriserTest.scala
@@ -3,8 +3,10 @@ package ae.teletronics.nlp.categorisation
 /**
   * Created by hhravn on 31/03/16.
   */
+
 import org.junit._
-import Assert._
+import Assert.assertThat
+import org.hamcrest.Matchers._
 import scala.collection.JavaConversions._
 
 
@@ -13,94 +15,79 @@ class CategoriserTest {
 
   @Test
   def testExactMatch() = {
-    val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new SimpleCategoriser(categories = List(cat))
+    val cat = Category("drugs", List("cocaine"))
+    val subj = new SimpleCategoriser(List(cat))
     val result = subj.categorise("cocaine")
 
-    assertNotNull(result)
-    assertTrue(result.length == 1)
-    assertTrue(result.contains(cat.name))
+    assertThat(result, contains(cat.name))
   }
 
   @Test
   def testThatUniqueCategoryIsReturned() = {
-    val cat1 = Category(name = "drugs1", words = List("cocaine"))
-    val cat2 = Category(name = "drugs2", words = List("smack"))
-    val cat3 = Category(name = "drugs3", words = List("heroin"))
-    val cat4 = Category(name = "drugs4", words = List("amphetamine"))
-    val subj = new SimpleCategoriser(categories = List(cat1, cat2, cat3, cat4))
+    val cat1 = Category("drugs1", List("cocaine"))
+    val cat2 = Category("drugs2", List("smack"))
+    val cat3 = Category("drugs3", List("heroin"))
+    val cat4 = Category("drugs4", List("amphetamine"))
+    val subj = new SimpleCategoriser(List(cat1, cat2, cat3, cat4))
     val result = subj.categorise("cocaine")
 
-    assertNotNull(result)
-    assertTrue(result.length == 1)
-    assertTrue(result.contains(cat1.name))
+    assertThat(result, contains(cat1.name))
   }
 
   @Test
   def testNegativeMatch() = {
-    val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new SimpleCategoriser(categories = List(cat))
+    val cat = Category("drugs", List("cocaine"))
+    val subj = new SimpleCategoriser(List(cat))
     val result = subj.categorise("sunshine")
 
-    assertNotNull(result)
-    assertTrue(result.length == 0)
+    assertThat(result.length, is(0))
   }
 
   @Test
   def testFuzzyMatch() = {
-    val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new FuzzyCategoriser(categories = List(cat), 1, 1)
+    val cat = Category("drugs", List("cocaine"))
+    val subj = new FuzzyCategoriser(List(cat), 1, 1)
     val result = subj.categorise("cocaino")
 
-    assertNotNull(result)
-    assertTrue(result.length == 1)
-    assertTrue(result.contains(cat.name))
+    assertThat(result, contains(cat.name))
   }
 
   @Test
   def testTooDistantMatch() = {
-    val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new FuzzyCategoriser(categories = List(cat), 2, 1)
+    val cat = Category("drugs", List("cocaine"))
+    val subj = new FuzzyCategoriser(List(cat), 2, 1)
     val result = subj.categorise("kocoino") // levenshtein distance of 3 from cocaine
 
-    assertNotNull(result)
-    assertTrue(result.length == 0)
+    assertThat(result.length, is(0))
   }
 
   @Test
   def testMultipleWordsMultipleCategoriesFuzzyMatch() = {
-    val cat1 = Category(name = "drugs1", words = List("cocaine"))
-    val cat2 = Category(name = "drugs2", words = List("smack"))
-    val cat3 = Category(name = "drugs3", words = List("heroin"))
-    val cat4 = Category(name = "drugs4", words = List("amphetamine"))
+    val cat1 = Category("drugs1", List("cocaine"))
+    val cat2 = Category("drugs2", List("smack"))
+    val cat3 = Category("drugs3", List("heroin"))
+    val cat4 = Category("drugs4", List("amphetamine"))
     val subj = new FuzzyCategoriser(List(cat1, cat2, cat3, cat4), 2, 1)
     val result = subj.categorise("cocaine smeck amphetaminex lalalala hiruen") // hiruen is distance 3 from heroin
 
-    assertNotNull(result)
-    assertTrue(result.length == 3)
-    assertTrue(result.contains(cat1.name))
-    assertTrue(result.contains(cat2.name))
-    assertTrue(result.contains(cat4.name))
+    assertThat(result, containsInAnyOrder(cat1.name, cat2.name, cat4.name))
   }
 
   @Test
   def testFailingPercentageMatch() = {
-    val cat1 = Category(name = "weapons", words = List("gun"))
+    val cat1 = Category("weapons", List("gun"))
     val subj = new FuzzyCategoriser(List(cat1), 3, 0.33)
     val result = subj.categorise("gut")
 
-    assertNotNull(result)
-    assertTrue(result.length == 0)
+    assertThat(result.length, is(0))
   }
 
   @Test
   def testSucceedingPercentageMatch() = {
-    val cat1 = Category(name = "weapons", words = List("gun"))
+    val cat1 = Category("weapons", List("gun"))
     val subj = new FuzzyCategoriser(List(cat1), 3, 0.34)
     val result = subj.categorise("gut")
 
-    assertNotNull(result)
-    assertTrue(result.length == 1)
-    assertTrue(result.contains(cat1.name))
+    assertThat(result, contains(cat1.name))
   }
 }
