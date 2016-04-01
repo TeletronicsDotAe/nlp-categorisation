9ab930713be3b125a381ff1a91332aa9d0e36a79
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-04-01 10:27:20 +0200

    FuzzyCategoriser has a maxDistancePercentageOfWordLength so we don't get spurious matches with short words

--- src/main/scala/ae/teletronics/nlp/categorisation/FuzzyCategoriser.scala
+++ src/main/scala/ae/teletronics/nlp/categorisation/FuzzyCategoriser.scala
@@ -4,8 +4,12 @@ import scala.math._
 
 /**
   * Created by Boris on 01-04-2016.
+  * MaxDistanceAsPercentageOfWordLength is a number between 0 and 1, both inclusive. It is there so that we can prevent e.g. 'Gun' from matching 'cat' or any other three-or-fewer letter words.
+  * The categoriser matches based on an 'AND' of maxDistance and MaxDistanceAsPercentageOfWordLength:
+  *   The distance between words has to be less than or equal to maxDistance AND
+  *   The distance between words over the length of one of the words has to be less than or equal to maxDistanceAsPercentageOfWordLength, for both words.
   */
-class FuzzyCategoriser(categories: java.util.List[Category], maxDistance: Int) extends Categoriser {
+class FuzzyCategoriser(categories: java.util.List[Category], maxDistance: Int, maxDistanceAsPercentageOfWordLength: Double) extends Categoriser {
 
   def categorise(text: String): java.util.List[String] = {
     import scala.collection.JavaConversions._
@@ -17,6 +21,15 @@ class FuzzyCategoriser(categories: java.util.List[Category], maxDistance: Int) e
   }
 
   private def fuzzyContains(words: Iterable[String], word: String): Boolean = {
-    words.exists(w => Levenshtein.distance(w, word) <= maxDistance)
+    words.exists(w => isFuzzyMatch(word, w))
+  }
+
+  private def isFuzzyMatch(word1: String, word2: String): Boolean = {
+    val distance = Levenshtein.distance(word1, word2)
+    val term0 = distance <= maxDistance
+    val term1 = distance.toDouble / word1.length() <= maxDistanceAsPercentageOfWordLength
+    val term2 = distance.toDouble / word2.length() <= maxDistanceAsPercentageOfWordLength
+
+    term0 && term1 && term2
   }
 }
--- src/test/scala/ae/teletronics/nlp/categorisation/CategoriserTest.scala
+++ src/test/scala/ae/teletronics/nlp/categorisation/CategoriserTest.scala
@@ -49,7 +49,7 @@ class CategoriserTest {
   @Test
   def testFuzzyMatch() = {
     val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new FuzzyCategoriser(categories = List(cat), 1)
+    val subj = new FuzzyCategoriser(categories = List(cat), 1, 1)
     val result = subj.categorise("cocaino")
 
     assertNotNull(result)
@@ -60,7 +60,7 @@ class CategoriserTest {
   @Test
   def testTooDistantMatch() = {
     val cat = Category(name = "drugs", words = List("cocaine"))
-    val subj = new FuzzyCategoriser(categories = List(cat), 2)
+    val subj = new FuzzyCategoriser(categories = List(cat), 2, 1)
     val result = subj.categorise("kocoino") // levenshtein distance of 3 from cocaine
 
     assertNotNull(result)
@@ -73,7 +73,7 @@ class CategoriserTest {
     val cat2 = Category(name = "drugs2", words = List("smack"))
     val cat3 = Category(name = "drugs3", words = List("heroin"))
     val cat4 = Category(name = "drugs4", words = List("amphetamine"))
-    val subj = new FuzzyCategoriser(List(cat1, cat2, cat3, cat4), 2)
+    val subj = new FuzzyCategoriser(List(cat1, cat2, cat3, cat4), 2, 1)
     val result = subj.categorise("cocaine smeck amphetaminex lalalala hiruen") // hiruen is distance 3 from heroin
 
     assertNotNull(result)
@@ -81,5 +81,15 @@ class CategoriserTest {
     assertTrue(result.contains(cat1.name))
     assertTrue(result.contains(cat2.name))
     assertTrue(result.contains(cat4.name))
+  }
+
+  @Test
+  def testPercentageMatch() = {
+    val cat1 = Category(name = "weapons", words = List("gun"))
+    val subj = new FuzzyCategoriser(List(cat1), 3, 0.99)
+    val result = subj.categorise("cat")
+
+    assertNotNull(result)
+    assertTrue(result.length == 0)
   }
 }
