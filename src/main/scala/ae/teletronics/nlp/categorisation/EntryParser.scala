b23fd97f57c7789ccf86b6513a6abb5d80053475
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-04-14 12:01:05 +0200

    Project builds, tests fail.

--- src/main/scala/ae/teletronics/nlp/categorisation/EntryParser.scala
+++ src/main/scala/ae/teletronics/nlp/categorisation/EntryParser.scala
@@ -8,9 +8,33 @@ import scala.util
 
 object EntryParser extends {
 
+  val regexMatcher = new RegexMatcher
+  val fuzzyMatcher = new FuzzyMatcher(1, 0.1)
+  val strictMatcher = new FuzzyMatcher(0, 0)
 
+  val signifierPattern = "([~/]?)"
+  val exactMatchPattern = "(.+)"
+  val optionalLabelPattern = "(/([^/]*))?"
+  val entryPattern = "^" + signifierPattern + exactMatchPattern + optionalLabelPattern + "$"
+  val entryParser = entryPattern.r("signifier", "exactMatch", "wholeLabel", "label")
 
   def parse(entry: String): EntryParseResult ={
+    val matchResult = entryParser.findFirstMatchIn(entry)
+    val optionParseResult = matchResult.map(m => {
+      val signifier = m.group("signifier")
+      val exactMatch = m.group("exactMatch").trim
+      val optionalLabel = m.group("label").trim
+      val label = if (optionalLabel.isEmpty) exactMatch else optionalLabel
 
+      val matcher = signifier match {
+        case "/" => regexMatcher
+        case "~" => fuzzyMatcher
+        case _   => strictMatcher
+      }
+
+      EntryParseResult(matcher, exactMatch, label)
+    })
+
+    optionParseResult.getOrElse(EntryParseResult(strictMatcher, entry, entry))
   }
 }
---
+++ src/test/scala/ae/teletronics/nlp/categorisation/EntryParseTest.scala
@@ -0,0 +1,8 @@
+package ae.teletronics.nlp.categorisation
+
+/**
+  * Created by Boris on 2016-04-14.
+  */
+class EntryParseTest {
+
+}
