fadc8fc0299d0851fdf7204af446669e5c0058aa
Trym Moeller <trym@tlt.local>
2016-09-28 13:35:01 +0200

    make domain classes json (de)serializable

--- src/main/scala/ae/teletronics/nlp/categorisation/Match.scala
+++ src/main/scala/ae/teletronics/nlp/categorisation/Match.scala
@@ -1,8 +1,10 @@
 package ae.teletronics.nlp.categorisation
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
   * Created by Boris on 2016-04-13.
   */
-case class Match(word: String) {
+case class Match(@JsonProperty("word") val word: String) {
   def getWord: String = word
 }
